c1aff4721310d230c229851d20026b3ed1c3d767
andrew bierman <94939237+crosschaincowboy@users.noreply.github.com>
2022-11-18 09:14:09 -0500

    troubleshooting error responses

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -690,7 +690,7 @@ router.post(
   '/:spotId/bookings',
   restoreUser,
   requireAuth,
-  // validateBookingBody,
+  validateBookingBody,
   async (req, res, next) => {
     const { user } = req;
 
