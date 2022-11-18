516acff0cdca55b16b06f76cdfddbc5c8e89c213
andrew bierman <94939237+crosschaincowboy@users.noreply.github.com>
2022-11-18 10:05:10 -0500

    troubleshooting error responses

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -4,7 +4,7 @@ const { validationResult } = require('express-validator');
 const myValidationResult = validationResult.withDefaults({
   formatter: error => {
     return {
-      myLocation: error.location,
+      // myLocation: error.location,
       statusCode: error.statusCode || 400
     };
   },
@@ -13,7 +13,8 @@ const myValidationResult = validationResult.withDefaults({
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
-  const validationErrors = myValidationResult(req);
+  // const validationErrors = myValidationResult(req);
+  const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
     const errors = validationErrors
