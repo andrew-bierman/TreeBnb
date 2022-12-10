f09776ed4b5a8a9352d339d087ae0cc09e4c2825
andrew bierman <94939237+crosschaincowboy@users.noreply.github.com>
2022-12-09 22:05:32 -0500

    navigation  progress

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,33 @@
-const ProfileButton = () => {
-    return (
-      <div style={{ color: "blue", fontSize: "100px" }}>
-        <i className="fas fa-solid fa-user"></i>
-      </div>
-    );
+// frontend/src/components/Navigation/ProfileButton.js
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
   };
 
-export default ProfileButton
+  const ulClassName = "profile-dropdown";
+
+  return (
+    <>
+      <button>
+        <i className="fas fa-user-circle" />
+      </button>
+      <ul className="profile-dropdown">
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default ProfileButton;
