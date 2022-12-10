0f8a6dacbf578dfa0d29283822178ed96452dfa6
andrew bierman <94939237+crosschaincowboy@users.noreply.github.com>
2022-12-09 22:02:27 -0500

    navigation  progress

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,9 @@
+const ProfileButton = () => {
+    return (
+      <div style={{ color: "blue", fontSize: "100px" }}>
+        <i className="fas fa-solid fa-user"></i>
+      </div>
+    );
+  };
+
+export default ProfileButton
