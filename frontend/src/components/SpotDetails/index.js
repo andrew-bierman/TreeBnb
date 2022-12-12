c98d369b70dbd04f652bab695eb22542decd7f82
andrew bierman <94939237+crosschaincowboy@users.noreply.github.com>
2022-12-12 15:18:28 -0500

    improvements to spot details page

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -27,18 +27,67 @@ const SpotDetailsComponent = () => {
         return state.spots.singleSpot
     })
 
+
+    let user = useSelector(state => {
+        // console.log(state)
+
+        return state.session.user
+    })
+
     if(!spot) {
         return null
     }
 
     const previewImage = spot.SpotImages.find( ({ preview }) => preview === true );
+
     let secondaryImages;
+    let secondaryImagesKeys;
+    let secondaryImagesValues
 
     if(previewImage){
-        secondaryImages = spot.SpotImages.find( ({ id }) => id !== previewImage.id );
+        secondaryImages = spot.SpotImages.filter( ({ id }) => id !== previewImage.id );
+
+        if(Object.keys(secondaryImages).length > 1){
+            // secondaryImages = Object.values(secondaryImages)
+            console.log('values ---', Object.values(secondaryImages))
+            // console.log('keys ---', Object.keys(secondaryImages))
+            secondaryImagesValues = Object.keys(secondaryImages).map(e => secondaryImages[e])
+            secondaryImagesKeys = Object.keys(secondaryImages)
+            // console.log('secondaryImagesKeys[1]', secondaryImagesKeys[1])
+        }
+
     }
 
-    console.log('spot details ----', {spot})
+    // console.log('spot details ----', {spot})
+    console.log('secondary images ----', secondaryImages, typeof secondaryImages)
+
+    console.log('secondary images values ----', secondaryImagesValues, 'isarray', Array.isArray(secondaryImagesValues))
+
+
+    let isLoggedIn
+    let isSpotOwner
+
+    if(!user){
+
+        isLoggedIn = false
+
+    } else {
+
+        isLoggedIn = true
+
+        if(user.id === spot.ownerId){
+            isSpotOwner = true
+        } else {
+            isSpotOwner = false
+        }
+    }
+
+    // if(Array.isArray(secondaryImagesValues)){
+    //     return (
+
+    //     )
+    // }
+
 
     return (
 
@@ -59,17 +108,24 @@ const SpotDetailsComponent = () => {
                         </div>
 
                         <div className='images'>
-                            {previewImage && (
-                                <img src={previewImage.url} alt='preview-image'></img>
+                            <div className='preview-image-container'>
+                                {previewImage && (
+                                    <img src={previewImage.url} alt='preview-image' className='preview-image'></img>
 
-                            )}
-                            {secondaryImages && (
-                                secondaryImages.map(image => {
-                                    <img key={image.id} src={image.url} alt='secondary-image'></img>
-                                })
-                            )}
+                                )}
+                            </div>
+
+                            <div className='secondary-images-container'>
+                                {(secondaryImagesValues.length > 0) && (
+                                    secondaryImagesValues.map(image => (
+                                        <img key={image.id} src={image.url} alt='secondary-image' className='secondary-image'></img>
+                                    ))
+                                )}
+                            </div>
 
                         </div>
+
+
 
                         <div className='host-details'>
                             {spot.Owner.firstName && (
@@ -77,21 +133,16 @@ const SpotDetailsComponent = () => {
                             )}
                         </div>
 
+                        {isSpotOwner && (
+                            <button>Edit this spot</button>
+                        )}
+
                     </div>
                 </div>
 
             )}
 
-            {/* {allSpots && allSpots.map(spot => (
-                <NavLink key={spot.id} to={`/spots/${spot.id}`}>
-                    <div key={spot.id}>
-                        <img src={spot.previewImage} alt='Preview Image'></img>
-                        <h4>{spot.name}</h4>
-                        <p>{spot.city}</p>
-                        <p>{`$${spot.price}`}</p>
-                    </div>
-                </NavLink>
-            ))} */}
+
 
         </div>
 
