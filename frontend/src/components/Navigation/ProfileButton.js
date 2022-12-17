63058b5a16f0a626b7ae94b88d4467abb360eea4
andrew bierman <94939237+crosschaincowboy@users.noreply.github.com>
2022-12-17 00:27:51 -0500

    conditional rendering of list your home button

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -46,6 +46,7 @@ function ProfileButton({ user }) {
     e.preventDefault();
     dispatch(sessionActions.logout());
     closeMenu();
+    history.push(`/`);
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
