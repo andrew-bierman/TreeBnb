41d2fd58f0c423347655b6435b1bb3664c057888
andrew bierman <94939237+crosschaincowboy@users.noreply.github.com>
2022-12-23 13:58:11 -0500

    improvements to edit review form

--- frontend/src/components/EditReviewForm/index.js
+++ frontend/src/components/EditReviewForm/index.js
@@ -1,217 +1,179 @@
+import React, { useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react'
-import { NavLink, Route, useParams, useHistory } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
-import { updateReview, getCurrentUserReviews, actionCreatorUpdateReview, actionCreatorResetUserReviews } from '../../store/reviews';
-import './EditReviewForm.css'
+import { updateReview, getCurrentUserReviews, actionCreatorResetUserReviews } from '../../store/reviews';
 
-import React, { useState } from 'react';
+import './EditReviewForm.css';
+
+const initialValues = {
+  review: "",
+  stars: 5
+};
+
+const initialTouchedValues = {
+  review: false,
+  stars: false
+};
 
 const EditReviewForm = () => {
-
   const dispatch = useDispatch();
-
-  const { reviewId } = useParams()
-
-  let user = useSelector(state => {
-    // console.log(state)
-
-    return state.session.user
-  })
-
-  let userReviews = useSelector(state => {
-    // console.log(state)
-
-    return state.reviews.user
-
-  })
-
-  useEffect(() => {
-
-    dispatch(getCurrentUserReviews())
-
-    return ( () => dispatch(actionCreatorResetUserReviews()))
-
-  }, [dispatch, reviewId])
-
-  // let findReview = Object.values(userReviews).find(review => review.id !== reviewId)
-  let findReview = userReviews[reviewId]
-
-
+  const { reviewId } = useParams();
   const history = useHistory();
 
-  const [review, setReview] = useState(findReview?.review || '');
-  const [stars, setStars] = useState(findReview?.stars || 5);
+  const [formValues, setFormValues] = useState(initialValues);
+  const [touched, setTouched] = useState(initialTouchedValues);
+  const [errors, setErrors] = useState(initialValues);
+  const [hoverRate, setHoverRate] = useState(0);
 
-  const updateReviewText = (e) => setReview(e.target.value);
-  const updateStars = (e) => setStars(e.target.value);
+  const user = useSelector(state => state.session.user);
+  const userReviews = useSelector(state => state.reviews.user);
 
-  const [errors, setErrors] = useState({});
+  const getColor = (index) => {
+    if (hoverRate >= index) return true;
 
-  const [shouldShowErrors, setShouldShowErrors] = useState(false);
+    if (!hoverRate && formValues.stars >= index) return true;
 
-  if(!findReview || findReview.userId !== user.id){
-    history.push('/user/current')
+    return false;
   }
 
+  const handleRate = (idx) => {
+    setFormValues(prevState => ({...prevState, stars: prevState.stars === idx ? 0 : idx}))
+  };
 
-  useEffect(() => {
-
-    if(userReviews && findReview){
-      console.log('hello', findReview)
-      setReview(findReview.review)
-      setStars(findReview.stars)
-    }
-
-  }, [userReviews])
+  const starRating = useMemo(() => {
+    return Array(5)
+      .fill(0)
+      .map((_, i) => i + 1)
+      .map((idx) => (
+        <div 
+          className='mr-1 cursor star-container'
+          onClick={() => handleRate(idx)} 
+          onMouseEnter={() => setHoverRate(idx)} 
+          onMouseLeave={() => setHoverRate(0)}
+        >
+          <span className={`fa fa-star ${getColor(idx) ? 'checked' : ''}`} />
+        </div>
+      ))
+  });
 
 
   const validateForm = () => {
-
-    // setErrors({})
-
-    const newErrors = {}
-
-    if (!review) {
-      newErrors.review = 'Review is required'
-
-    } else if ( review.length < 5){
-        newErrors.review = 'Review must be more than 5 characters'
-
-    } else {
-      newErrors.review = null
-      delete newErrors.review
-    }
-
-    if(!stars){
-      newErrors.lat = 'Stars input is required'
-
-    } else if ((parseFloat(stars) < 1) || (parseFloat(stars) > 5)){
-      newErrors.stars = 'Stars input is not valid'
-
-    } else {
-      newErrors.stars = null
-      delete newErrors.stars
-  }
-
     setErrors({
-      // ...errors,
-      ...newErrors
+      review: !formValues.review ? 'Review is required' : formValues.review.length < 5 ? 'Review must be more than 5 characters' : '',
+      stars: ((parseFloat(formValues.stars) < 1) || (parseFloat(formValues.stars) > 5)) ? 'Stars is not valid' : ''
     })
-
-    if(errors.length > 0) console.log(errors)
-
-
   };
-
-  useEffect(() => {
-    validateForm()
-  }, [
-      review,
-      stars
-  ])
-
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    // Submit the form to your API
+    const payload = {
+      reviewId,
+      review: formValues.review,
+      stars: formValues.stars,
+      User: user
+    };
 
-    // Validate the form
-    setShouldShowErrors(true);
-    validateForm();
+    console.log({payload})
 
+    let updatedReview = await dispatch(updateReview(payload))
 
-    console.log(Object.keys(errors).length)
-    // If there are no errors, submit the form to your API
-    if (Object.keys(errors).length === 0) {
-      // Submit the form to your API
-        const payload = {
-          reviewId,
-          review,
-          stars,
-          User: user
-        };
-
-        console.log({payload})
-
-        let updatedReview = await dispatch(updateReview(payload))
-
-        if (updatedReview) {
-          // history.push(`/reviews/${createdReview.id}`);
-          history.push(`/user/current`);
-        }
+    if (updatedReview) {
+      // history.push(`/reviews/${createdReview.id}`);
+      history.push(`/user/current`);
     }
   };
 
-  let isLoggedIn
+  const handleChange = async (event) => {
+    const { name, value } = event.target;
+    await setFormValues((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
 
-  if(!user){
-    return (
-      <div className='login-message'>
-        <p>Please login or signup to list your home</p>
-      </div>
-    )
-  } else {
-      isLoggedIn = true
+  const handleBlur = (event) => {
+    const { name } = event.target;
+    setTouched((prevState) => {
+      return {
+        ...prevState,
+        [name]: true,
+      };
+    });
   }
 
+  useEffect(() => {
+    const findReview = userReviews[reviewId];
 
+    if (!findReview || findReview.userId !== user.id) {
+      history.push('/user/current')
+    }
+
+    setFormValues({
+      review: findReview?.review,
+      stars: findReview?.stars
+    })
+  }, []);
+
+  // reset all errors to inital in the first render
+  useEffect(() => {
+    setErrors(initialValues);
+    setTouched(initialTouchedValues);
+  }, []);
+
+  useEffect(() => validateForm(), [formValues]);
+
+  useEffect(() => {
+    dispatch(getCurrentUserReviews());
+    return () => dispatch(actionCreatorResetUserReviews());
+  }, [dispatch, reviewId]);
+
+  if (!user) {
+    return (
+      <div className="is-flex is-justify-content-center w-100 mt-5">
+        <h5 className="title is-5">Please login or signup to continue</h5>
+      </div>
+    )
+  };
 
   return (
-
-    <div className='create-spot-page-component'>
-
-      {true && (
-
-        <div className='form-component'>
-
-          <h2>Edit a Review</h2>
-
-          {/* Display error messages for all fields */}
-          {Object.keys(errors).map(fieldName => {
-            const errorMessage = errors[fieldName];
-            if (shouldShowErrors && errorMessage) {
-              return <p key={errorMessage} className="error">{errorMessage}</p>;
-            }
-          })}
-
-          <div className='form-container'>
-
-            <form onSubmit={handleSubmit}>
-              <label className="name">Review:</label>
-              <input
-                type="textarea"
+    <div className='p-2 pt-5 is-flex is-flex-direction-column is-align-items-center'>
+      <h3 className="title is-3">Edit a Review</h3>
+        <div className='columns is-centered w-100'>
+          <form onSubmit={handleSubmit} className='is-flex is-flex-direction-column is-align-items-center column is-half'>
+            <div className="control has-icons-right w-100">
+              <textarea
                 id="review"
                 name="review"
-                value={review}
-                onChange={updateReviewText}
+                value={formValues.review}
+                className={`textarea ${errors.review && touched.review ? 'is-danger' : ''}`}
+                onChange={handleChange}
+                onBlur={handleBlur}
                 required
               />
-              {errors.review && <p>{errors.review}</p>}
+              {errors.review && touched.review && 
+                <>
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-exclamation-triangle"></i>
+                  </span>
+                  <h6 className="help is-danger">{errors.review}</h6>
+                </>
+              }
+            </div>
 
-              <label className="stars">Stars: {stars}</label>
-              <input
-                type="range"
-                min="1"
-                max="5"
-                className="slider"
-
-                id="stars"
-                name="stars"
-                value={stars}
-                onChange={updateStars}
-                required
-              />
-              {errors.stars && <p>{errors.stars}</p>}
-
-              <input type="submit" value="Submit"></input>
-
-            </form>
-
-          </div>
-
+            <div className='is-flex is-flex-direction-column is-align-items-center mt-4'>
+              <div className="is-flex mb-2">
+                {starRating}
+              </div>
+              <h6 className="subtitle is-6">Stars: {formValues.stars}</h6>
+              {errors.stars && <h6 className="help is-danger">{errors.stars}</h6>}
+            </div>
+            
+            <button type="submit" className="button is-primary mt-5 w-100">Submit</button>
+          </form>
         </div>
-      )}
-
     </div>
   );
 };
